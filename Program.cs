e6e45fa8fe2188204218ee1dc81aa2afc5054203
hajar298 <huseynova_hecer@mail.ru>
2023-02-25 13:44:49 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+//task1
+//Console.Write("Qiymet:");
+//int x = int.Parse(Console.ReadLine());
+//if (x >= 91 && x <= 100)
+//{
+//    Console.WriteLine("A");
+//}
+//else if (x >= 81 && x <= 90)
+//{
+//    Console.WriteLine("B");
+//}
+//else if (x >= 71 && x <= 80)
+//{
+//    Console.WriteLine("C");
+//}
+//else if (x >= 61 && x <= 70)
+//{
+//    Console.WriteLine("D");
+//}
+//else if (x >= 51 && x <= 60)
+//{
+//    Console.WriteLine("E");
+//}
+//else if (x <= 50)
+//{
+//    Console.WriteLine("Kesildiniz");
+//}
+//else
+//{
+//    Console.WriteLine("100 den yuxari qiymet olmur.");
+//}
+
+
+
+//task2
+//int[] numbers = { -1, 5, -2, 6, -7, -8, 9, 10, 15, -20, 25, 30, -18 };
+//int countP = 0, countN = 0;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > 0)
+//    {
+//        countP++;
+//    }
+//    else
+//    {
+//        countN++;
+//    }
+//}
+//Console.WriteLine(countP);
+//Console.WriteLine(countN);
+
+//task3
+//int[] numbers = { 1, 10, 20, 3, 50, -5, 60, -10, 15, 80 };
+//int max = numbers[0];
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] > max)
+//    {
+//        max = numbers[i];
+//    }
+
+//}
+//Console.WriteLine(max);
